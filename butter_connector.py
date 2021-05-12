ce2ae89ca34a0f9e3756f3491f6fa24b5e768c38
idanvidra <idan.vidra@gmail.com>
2021-05-12 14:56:34 +0300

    final changes to submission

--- butter_connector.py
+++ butter_connector.py
@@ -1,6 +1,6 @@
 from butter.mas.api import ClientFactory
 
-
+# connect to KIP using butter HTTP connector
 class ButterConnector:
     class __ButterConnector:
         def __init__(self, ip, connection_protocol='tcp'):
@@ -14,8 +14,6 @@ class ButterConnector:
     def __init__(self, ip, connection_protocol='tcp'):
         if not ButterConnector.instance:
             ButterConnector.instance = ButterConnector.__ButterConnector(ip, connection_protocol)
-            # else:
-            #   ButterConnector.instance.butterHttpClient = ClientFactory().getClient(ip, protocol=connection_protocol)
 
     def __getattr__(self, name):
         return getattr(self.instance, name)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from running_mode import *
 
 
 # choose running mode, options:
-# 1. optical flow
+# 1. optical flow - working but not as well as optical flow advanced - tests were on advanced
 # 2. tracking
 # 3. optical flow advanced
 rm =  Running_Mode()
